01ee26eac210068be9f2ad3c37cead06e1e6a9f7
erayxcx <154544339+erayxcx@users.noreply.github.com>
2024-07-24 22:27:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ client.once("ready", async() => {
   }
 });
 
-client.login(config.token).then(() => {
+client.login(process.env.token ).then(() => {
 	console.log(`[-] Discord API'ye istek gönderiliyor.`);
 	eval("console.clear()")
 }).catch(() => {
